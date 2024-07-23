b6556336439fabcf8c610898a4baf9e563ea43a0
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-07-23 20:33:26 +0200

    added projects-section, need more work, todo: ask for reference section and then implement the contact section

---
+++ src/app/main-content/projects-section/projects-section.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Project {
+  image: string;
+  projectNumber: string;
+  name: string;
+  tools: string;
+  description: string;
+  link: string
+}
+
+@Component({
+  selector: 'app-projects-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './projects-section.component.html',
+  styleUrl: './projects-section.component.scss'
+})
+export class ProjectsSectionComponent {
+
+  projects: Project[] = [
+    {
+      image: '/assets/img/projects/join.png',
+      projectNumber: '01/04',
+      name: 'Join',
+      tools: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and    categories.',
+      link: 'https://github.com/lehmand/join'
+    },
+
+    {
+      image: '/assets/img/projects/pollo-loco.png',
+      projectNumber: '02/04',
+      name: 'Pollo Loco',
+      tools: 'JavaScript | HTML | CSS',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      link: 'https://github.com/lehmand/el-pollo-loco'
+    }
+  ];
+
+
+  getProjectClass(index: number){
+    return index % 2 === 0 ? 'even-project' : 'odd-project'
+  }
+
+}
