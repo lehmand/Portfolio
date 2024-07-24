8cdae7ed3b48ff0853232e75151eb5a82c22a2d4
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-07-24 19:44:08 +0200

    added wip contact section, todo formfield and add reference section for later

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,14 +3,19 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 import { SkillsSectionComponent } from './skills-section/skills-section.component';
 import { ProjectsSectionComponent } from './projects-section/projects-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroSectionComponent, AboutMeSectionComponent, SkillsSectionComponent, ProjectsSectionComponent],
+  imports: [
+    HeroSectionComponent,
+    AboutMeSectionComponent,
+    SkillsSectionComponent,
+    ProjectsSectionComponent,
+    ContactSectionComponent,
+  ],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
