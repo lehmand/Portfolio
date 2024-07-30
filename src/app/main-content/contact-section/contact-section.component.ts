7fe60dd3b6f9364826aac1010690d7f6f22a94fd
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-07-30 19:58:23 +0200

    added contact form design, todo label animation

--- src/app/main-content/contact-section/contact-section.component.ts
+++ src/app/main-content/contact-section/contact-section.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact-section',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.scss'
 })
 export class ContactSectionComponent {
 
+
+  onSubmit() {
+    
+  }
 }
