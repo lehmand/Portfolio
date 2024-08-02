31f22fc5864197d40c815c31cfa630f026c1ac48
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-08-02 18:22:47 +0200

    updated angular

--- src/app/main-content/menu/menu.component.ts
+++ src/app/main-content/menu/menu.component.ts
@@ -1,14 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
+  animations: [
+    trigger('slideUpDown', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
+        animate('300ms ease-out', style({ transform: 'translateY(0%)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class MenuComponent {
 
-  playAnimation: boolean = false;
+  
+  
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../main-content/menu/menu.component';
 import { transition, trigger, style, animate } from '@angular/animations';
@@ -11,19 +11,18 @@ import { transition, trigger, style, animate } from '@angular/animations';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   animations: [
-    trigger('slideUpdown', [
-      transition('enter', [
-        style({ transform: 'translateY(100%'}),
+    trigger('slideUpDown', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)' }),
         animate('300ms ease-out', style({ transform: 'translateY(0%)' }))
       ]),
       transition(':leave', [
-        animate('300ms ease-in', style ({ transform: 'translateY(100%)' }))
+        animate('300ms ease-in', style({ transform: 'translateY(100%)' }))
       ])
     ])
   ]
 })
 export class HeaderComponent {
-
   isMenuOpen: boolean = false;
 
   toggleMenu(){
