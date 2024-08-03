bb5d1d80ee99763f0214cd7b300ff70c7a7f5eb1
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-08-03 16:39:01 +0200

    added burger animation

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../main-content/menu/menu.component';
 import { transition, trigger, style, animate } from '@angular/animations';
@@ -14,20 +14,50 @@ import { transition, trigger, style, animate } from '@angular/animations';
     trigger('slideUpDown', [
       transition(':enter', [
         style({ transform: 'translateY(100%)' }),
-        animate('300ms ease-out', style({ transform: 'translateY(0%)' }))
+        animate('250ms ease-out', style({ transform: 'translateY(0%)' }))
       ]),
       transition(':leave', [
-        animate('300ms ease-in', style({ transform: 'translateY(100%)' }))
+        animate('250ms ease-in', style({ transform: 'translateY(100%)' }))
       ])
     ])
   ]
 })
 export class HeaderComponent {
+
   isMenuOpen: boolean = false;
+  burgerImages: string[] = [
+    '/assets/icons/burger/burger1.png',
+    '/assets/icons/burger/burger2.png',
+    '/assets/icons/burger/burger3.png',
+    '/assets/icons/burger/burger4.png',
+    '/assets/icons/burger/burger5.png',
+    '/assets/icons/burger/burger6.png',
+    '/assets/icons/burger/burger7.png',
+    '/assets/icons/burger/burger8.png'
+  ];
+  currentIndex: number = 0;
+  currentImage: string = this.burgerImages[this.currentIndex];
+
+  constructor(){
+
+  }
+  
 
   toggleMenu(){
     this.isMenuOpen = !this.isMenuOpen;
+    this.playBurgerAnimation()
   }
 
+  playBurgerAnimation(){
+    let animationId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.burgerImages.length;
+      this.currentImage = this.burgerImages[this.currentIndex];
+      console.log(this.currentIndex)
+    }, 50);
+
+    setTimeout(() => {
+      clearInterval(animationId)
+    }, 400);
+  }
 
 }
