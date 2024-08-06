d9ab5c2f37f8f3293833f046e2b9c441c98b6a95
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-08-06 19:11:00 +0200

    todo myskills section

--- src/app/main-content/about-me-section/about-me-section.component.ts
+++ src/app/main-content/about-me-section/about-me-section.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me-section',
   standalone: true,
   imports: [],
   templateUrl: './about-me-section.component.html',
-  styleUrl: './about-me-section.component.scss'
+  styleUrl: './about-me-section.component.scss',
 })
 export class AboutMeSectionComponent {
+  constructor() {}
 
-  constructor(){}
+  isAnimated: boolean = false;
 
+  arrowToLeft: string[] = [
+    '/assets/icons/animations/arrow-to-left/arrow-to-left1.png',
+    '/assets/icons/animations/arrow-to-left/arrow-to-left2.png',
+    '/assets/icons/animations/arrow-to-left/arrow-to-left3.png',
+  ];
+
+  currentIndex: number = 0;
+  currentImage: string = this.arrowToLeft[this.currentIndex];
+
+  toggleAnimation() {
+    this.isAnimated = !this.isAnimated;
+    this.playAnimation();
+  }
+
+  playAnimation() {
+    const animationId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.arrowToLeft.length;
+      this.currentImage = this.arrowToLeft[this.currentIndex];
+      if(this.currentIndex === 2){
+        clearInterval(animationId);
+      }
+    }, 125);
+
+  }
 }
--- src/app/main-content/hero-section/hero-section.component.ts
+++ src/app/main-content/hero-section/hero-section.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements OnInit{
 
   email: string = "contact@daniel-lehmann.dev"
+
+  scrollDownArrow: string[] = [
+    '/assets/icons/animations/scroll-down/arrow-down1.png',
+    '/assets/icons/animations/scroll-down/arrow-down2.png',
+    '/assets/icons/animations/scroll-down/arrow-down3.png',
+    '/assets/icons/animations/scroll-down/arrow-down4.png',
+    '/assets/icons/animations/scroll-down/arrow-down5.png',
+    '/assets/icons/animations/scroll-down/arrow-down6.png',
+    '/assets/icons/animations/scroll-down/arrow-down7.png',
+  ];
+
+  currentIndex: number = 0;
+  currentImage: string = this.scrollDownArrow[this.currentIndex]
+
+  ngOnInit(): void {
+    this.arrowAnimation();
+  }
+  
+  arrowAnimation(){
+    let animationId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.scrollDownArrow.length;
+      this.currentImage = this.scrollDownArrow[this.currentIndex];      
+    }, 125);
+  }
 }
