46d54ee7780f0ff095858729af5db4c1972ae7f3
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-08-16 20:32:57 +0200

    everything done, only thing is php script

--- src/app/main-content/hero-section/hero-section.component.ts
+++ src/app/main-content/hero-section/hero-section.component.ts
@@ -8,31 +8,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
-export class HeroSectionComponent implements OnInit {
+export class HeroSectionComponent {
+  
   email: string = 'contact@daniel-lehmann.dev';
-
-  scrollDownArrow: string[] = [
-    '/assets/icons/animations/scroll-down/arrow-down1.png',
-    '/assets/icons/animations/scroll-down/arrow-down2.png',
-    '/assets/icons/animations/scroll-down/arrow-down3.png',
-    '/assets/icons/animations/scroll-down/arrow-down4.png',
-    '/assets/icons/animations/scroll-down/arrow-down5.png',
-    '/assets/icons/animations/scroll-down/arrow-down6.png',
-    '/assets/icons/animations/scroll-down/arrow-down7.png',
-  ];
-
-  currentIndex: number = 0;
-  currentImage: string = this.scrollDownArrow[this.currentIndex];
-  private animationId: any;
-
-  ngOnInit(): void {
-    this.arrowAnimation();
-  }
-
-  arrowAnimation() {
-    this.animationId = setInterval(() => {
-      this.currentIndex = (this.currentIndex + 1) % this.scrollDownArrow.length;
-      this.currentImage = this.scrollDownArrow[this.currentIndex];
-    }, 125);
-  }
+   
 }
