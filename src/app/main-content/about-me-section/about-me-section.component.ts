6417b48f1daf78dc47d2daea41e9e199635b2ae6
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-06-10 19:29:50 +0200

    refactored about me

--- src/app/main-content/about-me-section/about-me-section.component.ts
+++ src/app/main-content/about-me-section/about-me-section.component.ts
@@ -2,35 +2,50 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LanguageService } from '../../shared/language.service';
 import { Subscription } from 'rxjs';
 import { ABOUTMETRANSLATIONS } from '../../shared/translations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me-section.component.html',
   styleUrl: './about-me-section.component.scss',
 })
 export class AboutMeSectionComponent implements OnInit, OnDestroy {
-  constructor(private lang: LanguageService) {}
-
   isAnimated: boolean = false;
+  mobileArrowToLeft: string[] = [
+    '/assets/icons/arrow-mobile/arrow-to-left/first-arrow-toleft.png',
+    '/assets/icons/arrow-mobile/arrow-to-left/second-arrow-toleft.png',
+    '/assets/icons/arrow-mobile/arrow-to-left/third-arrow-toleft.png',
+  ];
   arrowToLeft: string[] = [
     '/assets/icons/animations/arrow-to-left/arrow-to-left1.png',
     '/assets/icons/animations/arrow-to-left/arrow-to-left2.png',
-    '/assets/icons/animations/arrow-to-left/arrow-to-left3.png',
-  ];
-  currentIndex: number = 0;
-  currentImage: string = this.arrowToLeft[this.currentIndex];
+    '/assets/icons/animations/arrow-to-left/arrow-to-left3.png'
+  ]
+  currentIndex: number = 2;
+  currentImage: string = this.mobileArrowToLeft[this.currentIndex];
+  isMobile: boolean = window.innerWidth <= 767;
+  translations: any = {};
   private animationId: any;
   private langSub: Subscription | undefined;
 
-  translations: any = {};
+  constructor(private lang: LanguageService) {}
 
   ngOnInit(): void {
-    this.langSub = this.lang.german$.subscribe(isGerman => {
-      this.translations = isGerman ? ABOUTMETRANSLATIONS.de : ABOUTMETRANSLATIONS.en;
+    this.langSub = this.lang.german$.subscribe((isGerman) => {
+      this.translations = isGerman
+        ? ABOUTMETRANSLATIONS.de
+        : ABOUTMETRANSLATIONS.en;
     });
-    this.translations = this.lang.isGerman() ? ABOUTMETRANSLATIONS.de : ABOUTMETRANSLATIONS.en;
+    this.translations = this.lang.isGerman()
+      ? ABOUTMETRANSLATIONS.de
+      : ABOUTMETRANSLATIONS.en;
+    window.addEventListener('resize', this.resizeHandler);
+    window.addEventListener('resize', () => {
+      this.currentIndex = this.isMobile ? 2 : 0
+      this.currentImage = this.isMobile ? this.mobileArrowToLeft[this.currentIndex] : this.arrowToLeft[this.currentIndex]
+    })
   }
 
   ngOnDestroy(): void {
@@ -38,18 +53,22 @@ export class AboutMeSectionComponent implements OnInit, OnDestroy {
       this.langSub.unsubscribe();
     }
   }
- 
- 
- toggleAnimation() {
+
+  resizeHandler = () => {
+    this.isMobile = window.innerWidth <= 767;
+  }
+
+  toggleAnimation() {
     this.isAnimated = !this.isAnimated;
     this.playAnimation();
   }
 
   playAnimation() {
     const animationId = setInterval(() => {
-      this.currentIndex = (this.currentIndex + 1) % this.arrowToLeft.length;
+      this.currentIndex =
+        (this.currentIndex + 1) % this.arrowToLeft.length;
       this.currentImage = this.arrowToLeft[this.currentIndex];
-      if(this.currentIndex === 2){
+      if (this.currentIndex === 2) {
         clearInterval(animationId);
       }
     }, 125);
